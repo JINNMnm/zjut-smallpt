b15a6249855ac27545554366c9f5fa5824ff38e9
JINNM <JINNMnm1993@gmail.com>
2025-05-26 11:49:51 +0800

    initial commit

---
+++ cpp-smallpt/build/CMakeFiles/smallpt_cpu.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/ssd10/jxc/smallpt/cpp-smallpt/src/cpp-smallpt.cpp" "CMakeFiles/smallpt_cpu.dir/src/cpp-smallpt.cpp.o" "gcc" "CMakeFiles/smallpt_cpu.dir/src/cpp-smallpt.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp-smallpt/build/CMakeFiles/smallpt_cpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smallpt_cpu.dir/src/cpp-smallpt.cpp.o"
+  "CMakeFiles/smallpt_cpu.dir/src/cpp-smallpt.cpp.o.d"
+  "smallpt_cpu"
+  "smallpt_cpu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smallpt_cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
